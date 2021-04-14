5e40269915957e2af3f538c29932a0f0689ad094
EmiPro97 <emi.proie97@gmail.com>
2021-04-14 20:44:55 +0200

    added js task 3

--- js/script.js
+++ js/script.js
@@ -31,11 +31,11 @@ $(document).ready(function() {
     var addToList = $('.add-to-list');
     var list = $('.toDo-list');
     var template = $('#template li');
-
-
+    
+    
     // 1. Fill the list
     for (var i = 0; i < listItems.length; i++) {
-
+        
         // Cloning template
         myClone = template.clone();
         myClone.find('.text').text(listItems[i].toDoItem);
@@ -43,28 +43,34 @@ $(document).ready(function() {
         // Adding to list
         list.append(myClone);
     }
-
-
+    
+    
     // 2. Add a new item to the list
     addToList.keyup(function(e){
-
+        
         // if the user press enter
         if (e.which == 13){
             var addValue = addToList.val().trim();
-       
+            
             // if the input is valid
             if (addValue != '') {
                 myClone = template.clone();
                 myClone.find('.text').text(addValue);
-    
+                
                 // Adding to list the new item
                 list.append(myClone);
-    
+                
                 // Reset
                 addToList.val('');
             }
         }
         
+    });
+    
+    
+    // 3. Remove list item on icon click
+    $('body').on('click', '.toDo-list li i', function(){
+        $(this).parent().remove();
     });
 
 
